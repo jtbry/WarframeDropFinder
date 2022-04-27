9f02f7e156c7b883809505c71d02250054e055b8
jtbry <justintbry@gmail.com>
2022-04-27 02:09:33 -0400

    fix: add datafetcher to deploy-all script

--- deploy/deploy-all.sh
+++ deploy/deploy-all.sh
@@ -5,11 +5,13 @@ set -e
 echo -e "Building\n----"
 ./api/build.sh
 ./client/build.sh
+./datafetcher/build.sh
 
 # Deploy apps
 echo -e "----\nDeploying\n----"
 ./api/deploy.sh
 ./client/deploy.sh
+./datafetcher/deploy.sh
 
 # Remove built files
 if [ "$1" != "--keep-build" ]; then
