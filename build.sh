533bea4a65ee4ca495241aa26ef109748dffa0eb
jtbry <justintbry@gmail.com>
2022-04-29 20:54:13 -0400

    fix: build script datafetcher typo

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 echo -e "----\nBuilding DataFetcher image\n----"
-docker build ./src/ -f ./src/Wfdf.DataFetcher/Dockerfile -t datafetch
+docker build ./src/ -f ./src/Wfdf.DataFetcher/Dockerfile -t datafetcher
 echo -e "----\nBuilding docker-compose services\n----"
 docker-compose build
