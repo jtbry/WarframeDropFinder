b84cfeb2a7cace71c6addf7314e4e793818269e9
jtbry <justintbry@gmail.com>
2022-05-05 23:26:27 -0400

    refactor: fix Item.cs formatting

--- src/Wfdf.Core/Models/Item.cs
+++ src/Wfdf.Core/Models/Item.cs
@@ -5,15 +5,15 @@ namespace Wfdf.Core.Models;
 [BsonIgnoreExtraElements]
 public class Item
 {
-  public string uniqueName { get; set; } = string.Empty;
-  public string name { get; set; } = string.Empty;
-  public string description { get; set; } = "N/A";
-  public string category { get; set; } = string.Empty;
-  public bool tradable { get; set; } = false;
-  public string wikiaUrl { get; set; } = string.Empty;
-  public string imageName { get; set; } = string.Empty;
-  public bool vaulted { get; set; } = false;
-  public IEnumerable<Patchlog> patchlogs { get; set; } = new List<Patchlog>();
-  public IEnumerable<DropSource> drops { get; set; } = new List<DropSource>();
-  public IEnumerable<Component> components { get; set; } = new List<Component>();
+    public string uniqueName { get; set; } = string.Empty;
+    public string name { get; set; } = string.Empty;
+    public string description { get; set; } = "N/A";
+    public string category { get; set; } = string.Empty;
+    public bool tradable { get; set; } = false;
+    public string wikiaUrl { get; set; } = string.Empty;
+    public string imageName { get; set; } = string.Empty;
+    public bool vaulted { get; set; } = false;
+    public IEnumerable<Patchlog> patchlogs { get; set; } = new List<Patchlog>();
+    public IEnumerable<DropSource> drops { get; set; } = new List<DropSource>();
+    public IEnumerable<Component> components { get; set; } = new List<Component>();
 }
